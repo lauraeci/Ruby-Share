e3d4923b9ffa880d625a0c715cc3a7d02098fe90
lauraeci <lmarshall@empowered.com>
2012-12-06 01:35:37 -0800

    Fixed combo example buying two items from the same combo

--- Ruby-SHare/program/restaurant.rb
+++ Ruby-SHare/program/restaurant.rb
@@ -6,27 +6,40 @@ class Restaurant
     @menu_choices = menu_choices
     @restaurant_ids = []
     self.read_menu
-    all_options, is_choice_found, is_combo_item = self.find_options
+    all_options, is_choice_found = self.find_options
     
+    @answer = nil
     if all_options.nil?
       puts nil
-      return nil
+      return @answer
     else
       cheapest_option, restaurant = self.show_best_option all_options
       
-      @menu_choices.each_with_index {|choice, index|
-        is_combo_item[index]
-        if is_choice_found[index] == @menu_choices.length
-          puts restaurant.to_s + ',' + cheapest_option.to_s
-          return restaurant.to_s, cheapest_option.to_s
-        end
-      }
-      puts nil
-      return nil
+      # check all the choices are found at that restaurant
+      if is_choice_found[restaurant] == (@menu_choices.length)
+        puts restaurant.to_s + ',' + cheapest_option.to_s
+        @answer = restaurant.to_s, cheapest_option.to_s
+      end
+         
     end
     
+    return @answer
   end
   
+  ##
+  #nicely display the answer
+  ##
+  def to_s
+    @answer
+  end
+  
+  ## read_menu:
+  #   
+  #   parse the menu cvs file @menu_file 
+  #   
+  #   @postcondition: @menu_items contains a list of items as a hash with restaurant as the key
+  #   for each restaurant.
+  ##
   def read_menu
     
     if (@menu_file.nil?)
@@ -35,6 +48,7 @@ class Restaurant
     
     menu = File.open(@menu_file).readlines
     @menu_items = Hash.new
+    
     menu.each {|item| 
       menu_item = item.split(",")
       
@@ -46,85 +60,116 @@ class Restaurant
       end
       items = []
       
+      item_type = "items"
+      combo_number = 0
+      
+      if (menu_item.length > 3)
+        item_type = "combo"
+        combo_number += 1
+      end
+      
       for i in 2...menu_item.length
         items << menu_item[i].strip
       end
       
-      @menu_items[menu_item[0].to_i] << {"price" => menu_item[1].strip.to_f, "items" => items}
+      @menu_items[menu_item[0].to_i] << {"price" => menu_item[1].strip.to_f, item_type => items, "combo_number"=> combo_number}
       
     }
-    #puts @menu_items[2]["price"]
-    puts @menu_items.inspect
-  end
-  
-  def find_options
-    puts @restaurant_ids.inspect
-    options = []
-    is_choice_found_count = []
-    is_combo_item = []
     
-    @menu_choices.each_with_index {|choice, index|
-      if (is_choice_found_count[index].nil?)
-        is_choice_found_count[index] = 0
-      end
+  end
+  
+  ## 
+  #  find_options:
+  #  find the restaurant options for the @menu_choices
+  # 
+  #  @return 
+  #    options:  options is a list of the price of the order from each restaurant
+  #    is_choice_found_count: list of the count of items for each restaurant
+  #  
+    def find_options
       
-      if (is_combo_item[index].nil?)
-        is_combo_item[index] = 0
-      end
+      options = []
+      is_choice_found_count = []
+      purchased_items = []
       
-      @restaurant_ids.each{|id|
+      @menu_choices.each_with_index {|choice, index|
         
-        @menu_items[id].each {|item|
+        if (purchased_items[index].nil?)
+          purchased_items[index] = []
+        end
+        
+        @restaurant_ids.each{|restaurant_id|
           
-          if (options[id].nil?)
-            options[id] = 0.0
+          if (is_choice_found_count[restaurant_id].nil?)
+            is_choice_found_count[restaurant_id] = 0
           end
           
-          if (item["items"].find{|anItem| choice == anItem})
+          @menu_items[restaurant_id].each {|item|
             
-            
-            if (is_combo_item[index] != index)
-              puts 'Is Not combo: ' + id.to_s + ": " + choice + ":" + item["price"].to_s
-              cost = options[id]
-              cost += item["price"]
-              options[id] = cost
-               puts is_combo_item[index]
-              is_combo_item[index] = 0
-             
-            else
-              
-              puts 'Is Combo: ' +  id.to_s + ": " + choice + ":" + item["price"].to_s
+            if (options[restaurant_id].nil?)
+              options[restaurant_id] = 0.0
             end
-            is_choice_found_count[index] += 1 
-            is_combo_item[index] = index
-            puts index
-         
             
-          end
-        }
-      }     
-    }
-    puts 'options: ' + options.inspect
-    puts 'count: ' + is_choice_found_count.inspect
-    return options, is_choice_found_count, is_combo_item
+            # Is the choice a regular item?
+            if (!item["items"].nil? and item["items"].include? choice)
+              
+              cost = options[restaurant_id]
+              cost += item["price"]
+              options[restaurant_id] = cost
+              is_choice_found_count[restaurant_id] += 1 
+              
+              # Is the choice a combo item?
+            elsif (!item["combo"].nil?)
+              
+              if (item["combo"].include? choice)
+                
+                if !purchased_items.include? choice
+                  
+                  purchased_items[index] << item["combo"]
+                  purchased_items = purchased_items.flatten
+                  cost = options[restaurant_id]
+                  cost += item["price"]
+                  options[restaurant_id] = cost
+                  
+                end
+                
+                is_choice_found_count[restaurant_id] += 1 
+                
+              end
+              
+            end
+            
+            
+          }
+        }     
+      }
+      
+      
+      return options, is_choice_found_count
+    end
+    
+    ##
+    # show_best_option
+    # @return:
+    # cheapest_option: price of items from the cheapest restaurant option
+    # restaurant: the id from the cheapest restaurant
+    ##
+    def show_best_option options
+      cheapest_option = Float::MAX
+      restaurant = -1
+      options.each_with_index {|option, index| 
+        if (!option.nil? and option < cheapest_option)
+          cheapest_option = option 
+          restaurant = index
+        end
+      }
+      return cheapest_option, restaurant
+    end
+    
   end
   
-  def show_best_option options
-    cheapest_option = Float::MAX
-    restaurant = -1
-    options.each_with_index {|option, index| 
-      if (!option.nil? and option < cheapest_option)
-        cheapest_option = option 
-        restaurant = index
-      end
-    }
-    return cheapest_option, restaurant
-  end
+  # menu_file = ARGV[0]
+  # ARGV.shift
+  # menu_choices = ARGV
   
-end
-
-menu_file = ARGV[0]
-ARGV.shift
-menu_choices = ARGV
-
-familiar_faces_restaurants = Restaurant.new(menu_file, menu_choices)
+  # familiar_faces_restaurants = Restaurant.new(menu_file, menu_choices)
--- Ruby-SHare/program/restaurant_test.rb
+++ Ruby-SHare/program/restaurant_test.rb
@@ -2,31 +2,32 @@ require 'rspec'
 require './restaurant.rb'
 
 describe Restaurant do 
-
-  before do
-    # @restaurant = Restaurant.new
-  end
   
-  it "should work for menu 1 " do 
+  it "TEST 1: should work for menu 1 " do 
    #menus1.csv ham_sandwich burrito
    #2, 11.5
    answer = Restaurant.new 'menu1.csv', ["ham_sandwich", "burrito"]
-   answer.to_s.should == "2, 11.5"
+   answer.to_s.should == ["2", "11.5"]
   end
 
-  it "should work for menu 2" do
+  it "TEST 2: should work for menu 2" do
     answer = Restaurant.new 'menus2.csv', ["ham_sandwich", "burrito"]
     answer.to_s.should == nil
   end 	
 
-  it "should give us what we expect for menus3.csv" do
-    @restaurant = Restaurant.new 'menus3.csv',["milkshake", "fish_sandwich"]
-    @restaurant.to_s.should == "6, 11.0"
+  it "TEST 3: should give us what we expect for menus3.csv" do
+    answer = Restaurant.new 'menus3.csv',["milkshake", "fish_sandwich"]
+    answer.to_s.should == ["6", "11.0"]
   end
 
-  it "should give us the right order when the order is a little more complex than our examples " do
-    @restaurant = Restaurant.new 'menus3.csv',["milkshake", "fish_sandwich", "blue_berry_muffin"]
-    @restaurant.to_s.should == "6, 11.0"
+  it "TEST 4: should give us the right order when the order 2 items from the same combo " do
+    answer = Restaurant.new 'menus3.csv',["milkshake", "fish_sandwich", "blue_berry_muffin"]
+    answer.to_s.should == ["6", "11.0"]
+  end
+  
+  it "TEST 5: should give us the right order when the order is 2 items requiring 2 combos -- didn't get this case implemented " do
+    answer = Restaurant.new 'menus3.csv',["fish_sandwich", "blue_berry_muffin", "chocolate_milk","fish_sandwich", "blue_berry_muffin", "chocolate_milk"]
+    answer.to_s.should == ["6", "12.0"]
   end
 
 end
